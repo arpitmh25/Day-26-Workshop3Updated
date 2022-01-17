705ff529cff5c5119e6b57f503f8507b400ba41b
Arpit <arpitmh23@gmail.com>
2022-01-17 11:32:15 +0530

    Adding Uc3

---
+++ src/main/java/com/bridgelabz/workshop3/HotelRegistrationInterface.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.workshop3;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public interface HotelRegistrationInterface {
+
+    public void addHotel(String hotelName, int rating, double weekdayRegularCustomerCost, double weekendRegularCustomerCost);
+
+    public int getHotelListSize();
+
+    public void printHotelList();
+
+    public ArrayList<Hotel> getHotelList();
+
+    public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate);
+
+}
---
+++ src/main/java/com/bridgelabz/workshop3/HotelReservationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.workshop3;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to Hotel Reservation System");
+    }
+}
