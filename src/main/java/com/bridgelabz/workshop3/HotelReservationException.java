f49bd1f97be7c14244b02df49964e9cfb6373f4f
Arpit <arpitmh23@gmail.com>
2022-01-17 11:14:19 +0530

    Implementing program

---
+++ src/main/java/com/bridgelabz/workshop3/HotelReservationException.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.workshop3;
+
+
+public class HotelReservationException extends RuntimeException {
+
+    enum ExceptionType {
+        ENTERED_NULL, ENTERED_EMPTY;
+    }
+
+    ExceptionType type;
+
+    public HotelReservationException(ExceptionType type, String message) {
+
+        super(message);
+        this.type = type;
+
+    }
+}
